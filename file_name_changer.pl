e32f8989df6048d42540ecbc812c48ed7c29acae
Ali Denno <aliedenno@gmail.com>
2019-12-26 01:38:32 +0100

    Update file name changer

--- file_name_changer.pl
+++ file_name_changer.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl -w
 
+# A script that changes a file name by stripping it from the ID which looks something like this '-dbIwxl1HJsw'
 # Author: Ali Denno
 
 my @files = <*>;
 foreach my $file (@files) {
   my $regex = qr/(-)(?!.*\1).+(?=.mp3)/mp;
   my $originalname = $file;
-  if ( $file =~ /$regex/g ) {
-	$file =~ s/${^MATCH}//ig;
-	rename "$originalname", "$file" or die "Cannot rename file: $originalname!";
+  if ( ! -d $file ) {
+    if ( $file =~ /$regex/g ) {
+      $file =~ s/${^MATCH}//ig;
+      rename "$originalname", "$file" or die "Cannot rename file: $originalname!";
+    }
   }
 }
