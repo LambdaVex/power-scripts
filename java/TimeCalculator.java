5d5a82b7548c885d8120f46f2281e0ffecc08c9c
Ali Denno <aliedenno@gmail.com>
2020-05-04 13:49:47 +0200

    Update TimeCalculator.java

--- java/TimeCalculator.java
+++ java/TimeCalculator.java
@@ -1,6 +1,8 @@
 import java.time.Duration;
 import java.time.LocalTime;
-
+/*
+import java.time.Duration;import java.time.LocalTime;System.out.println(">_ " +  LocalTime.of(7,30).plus(Duration.ofHours(8).plusMinutes(21))); // or System.out.println("at  : " +  start.plusHours(8).plusMinutes(21));  
+*/
 public class TimeTest {
     public static void main(String[] args) {
         System.out.println(">_ " +  LocalTime.of(7,30).plus(Duration.ofHours(8).plusMinutes(21))); // or System.out.println("at  : " +  start.plusHours(8).plusMinutes(21));
