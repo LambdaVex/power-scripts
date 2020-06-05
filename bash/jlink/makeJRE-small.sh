288fcd38177891f1adc65a19d0df10cd7bc21aea
Ali Denno <aliedenno@gmail.com>
2020-06-05 14:00:58 +0200

    Update makeJRE-small.sh

--- bash/jlink/makeJRE-small.sh
+++ bash/jlink/makeJRE-small.sh
@@ -2,7 +2,7 @@ rm -rf out/jre
 $JAVA_HOME/bin/jlink \
 	--module-path $JAVA_HOME/jmods:out/production \
 	--add-modules business.stockapp.ui \
-	--launcher JOKER=business.stockapp.ui/business.stockapp.ui.Main \
+	--launcher CoolLauncher=business.stockapp.ui/business.stockapp.ui.Main \
 	--compress 2	\
 	--no-header-files	\
 	--no-man-pages	\
