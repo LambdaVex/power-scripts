e00235acae0515565b36da45b6d99fe722a9cdd4
Ali Denno <aliedenno@gmail.com>
2020-06-05 13:59:19 +0200

    Create makeJRE.sh

---
+++ bash/jlink/makeJRE.sh
@@ -0,0 +1,6 @@
+rm -rf out/jre
+$JAVA_HOME/bin/jlink \
+	--module-path $JAVA_HOME/jmods:out/production \
+	--add-modules business.stockapp.ui \
+	--launcher JOKER=business.stockapp.ui/business.stockapp.ui.Main \
+	--output out/jre
