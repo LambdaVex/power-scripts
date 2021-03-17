2103a3bb1dbcd47952dff9aae96200b6fda9f35a
Ali Denno <aliedenno@gmail.com>
2021-03-17 13:19:10 +0100

    Update data_backup.sh

--- bash/data_backup.sh
+++ bash/data_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar -cvzf backup.tar.gz ~/{Documents,Downloads,Desktop,Pictures,Videos}
+tar -cvzf ~/Desktop/backup.tar.gz ~/{Documents,Downloads,Desktop,Pictures,Videos}
 # or
 # tar -czf backup.tar.gz ~/{Documents,Downloads,Desktop,Pictures,Videos} 2>/dev/null
 # means: redirect standard error to /dev/null. /dev/null is a special device that discards everything that is written to it
