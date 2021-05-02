e3fa57d466c5776e6ecff6539576a771c4aa134a
Ali Denno <aliedenno@gmail.com>
2021-05-02 14:23:47 +0200

    Create updater.sh

---
+++ bash/updater.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade  && sudo apt autoremove && sudo apt autoclean && sudo apt clean && sudo apt update && sudo apt autoremove -y && sudo apt autoclean && sudo apt full-upgrade -y && sudo apt-get clean && sudo apt-get autoclean && sudo apt-get autoremove
