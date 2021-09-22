c84d8cfc94d842cb354c7b7ac8160c45e1b3f288
Ali Denno <aliedenno@gmail.com>
2021-09-22 10:15:19 +0200

    Update time-calculator.py

--- python/time-calculator.py
+++ python/time-calculator.py
@@ -1,15 +1,15 @@
 import datetime
 
-soll = datetime.timedelta(hours= 7 , minutes=36, seconds=00)
-pause = datetime.timedelta(hours= 00, minutes=45, seconds=00)
+basis = datetime.timedelta(hours=7, minutes=36, seconds=00)
+pause = datetime.timedelta(hours=00, minutes=45, seconds=00)
 
-start = datetime.timedelta(hours= 7 , minutes=15, seconds=00)
-end = datetime.timedelta(hours= 13, minutes=10, seconds=00)
+start = datetime.timedelta(hours=8, minutes=30, seconds=00)
+end = datetime.timedelta(hours=17, minutes=10, seconds=00)
 
 print(end - start - pause)
 
-if soll < (end - start - pause):
-    print((end - start - pause) - soll)
+if basis < (end - start - pause):
+    print((end - start - pause) - basis)
 else:
     print("-", end="")
-    print(soll - (end - start - pause))
+    print(basis - (end - start - pause))
