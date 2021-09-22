efb1245b1f6a25b7e92e281804fa19a5966403bb
Ali Denno <aliedenno@gmail.com>
2021-09-22 11:00:38 +0200

    Update time-calculator.py
    closes #1

--- python/time-calculator.py
+++ python/time-calculator.py
@@ -1,20 +1,26 @@
 import datetime
 
-basis = datetime.timedelta(hours=7, minutes=36, seconds=00)
-pause = datetime.timedelta(hours=00, minutes=45, seconds=00)
+def calculate_info(start, end, pause, basis, shortday):
+    if shortday > (end - start - pause):
+        pause = datetime.timedelta(hours=00, minutes=00, seconds=00)
+        print("~Short Day~")
 
-start = datetime.timedelta(hours=8, minutes=30, seconds=00)
-end = datetime.timedelta(hours=17, minutes=30, seconds=00)
+    print("Started at: ", start)
+    print("Finished at: ", end)
+    print("************************")
+    print("Time spent: ", end - start - pause)
+    print("SALDO: ", end="")
+    if basis < (end - start - pause):
+        print((end - start - pause) - basis)
+    else:
+        print("-", end="")
+        print(basis - (end - start - pause))
+    print("************************")
 
-print("Started at: ", start)
-print("Finished at: ", end)
-print("************************")
 
-print("Time spent: ", end - start - pause)
-print("SALDO: ", end="")
-if basis < (end - start - pause):
-    print((end - start - pause) - basis)
-else:
-    print("-", end="")
-    print(basis - (end - start - pause))
-print("************************")
+calculate_info(datetime.timedelta(hours=7, minutes=30, seconds=00),  # start
+               datetime.timedelta(hours=17, minutes=00, seconds=00),  # end
+               datetime.timedelta(hours=00, minutes=45, seconds=00),  # pause
+               datetime.timedelta(hours=7, minutes=36, seconds=00),  # basis
+               datetime.timedelta(hours=6, minutes=00, seconds=00)  # shortday
+               )
