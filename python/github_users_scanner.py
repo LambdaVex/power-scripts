e5441e5d82d5264cba8f753f6751427d4a065df8
Ali Denno <aliedenno@gmail.com>
2022-02-21 11:41:48 +0100

    Add Authorization support

--- python/github_users_scanner.py
+++ python/github_users_scanner.py
@@ -1,22 +1,40 @@
-# Be aware that you might the following error, if you use the api frequently 
-# {'message': "API rate limit exceeded. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)", 'documentation_url': 'https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting'}
-
 import requests
 
-followers_data = requests.get("https://api.github.com/users/[USER]/followers?per_page=2000").json()
-following_data = requests.get("https://api.github.com/users/[USER]/following?per_page=2000").json()
+mode = input("Would you like to use API-Tokens in the request (1:yes / 2:no ): ").lower()
 
 followers_list = []
 following_list = []
+followers_data_url = "https://api.github.com/users/[USER]/followers?per_page=2000"
+following_data_url = "https://api.github.com/users/[USER]/following?per_page=2000"
 
-for value in followers_data:
-    followers_list.append(str(value.get('login')))
-for value in following_data:
-    following_list.append(str(value.get('login')))
 
-print("Out of the " + str(
-    len(following_data)) + " user(s) you are following, the following user(s) are not following you back: ")
-print(list(set(following_list).difference(set(followers_list))))
+def show_data(followers, following):
 
-print("Out of the " + str(len(followers_data)) + " user(s) following you, you are not following these user(s) back: ")
-print(list(set(followers_list).difference(set(following_list))))
+    for value in followers:
+        followers_list.append(str(value.get('login')))
+    for value in following:
+        following_list.append(str(value.get('login')))
+    print("Out of the " + str(
+        len(following)) + " user(s) you are following, the following user(s) are not following you back: ")
+    print(list(set(following_list).difference(set(followers_list))))
+    print(
+        "Out of the " + str(len(followers)) + " user(s) following you, you are not following these user(s) back: ")
+    print(list(set(followers_list).difference(set(following_list))))
+
+
+if mode == "1":
+    print('Request with authorization was selected ...')
+    headers = {'Authorization': '[TOKEN]'}
+
+    followers_data = requests.get(followers_data_url, headers=headers).json()
+    following_data = requests.get(following_data_url, headers=headers).json()
+
+    show_data(followers_data, following_data)
+elif mode == "2":
+    print('Request without authorization was selected ...')
+    followers_data = requests.get(followers_data_url).json()
+    following_data = requests.get(following_data_url).json()
+
+    show_data(followers_data, following_data)
+else:
+    print('choice is not recognized!')
