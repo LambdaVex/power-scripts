ce18ebefa2f76b0a518ab548d87a4395fa78ed0b
Ali Denno <aliedenno@gmail.com>
2023-07-25 21:03:32 +0200

    Update time_calculator.py

--- python/time_calculator.py
+++ python/time_calculator.py
@@ -4,6 +4,7 @@ PAUSE = datetime.timedelta(hours=00, minutes=45, seconds=00)
 BASIS = datetime.timedelta(hours=7, minutes=36, seconds=00)
 SHORTDAY = datetime.timedelta(hours=6, minutes=00, seconds=00)
 
+
 def calculate_info(start, end, pause, basis, shortday):
     if shortday > (end - start - pause):
         pause = datetime.timedelta(hours=00, minutes=00, seconds=00)
@@ -21,15 +22,18 @@ def calculate_info(start, end, pause, basis, shortday):
         print(basis - (end - start - pause))
     print("************************")
 
+
 def calculate_leave(start, pause, basis, shortday):
     print("Started at:", start)
-    print("For a Normal Day, you should finish min at:", start + pause +basis)
+    print("For a Normal Day, you should finish min at:", start + pause + basis)
     print("For a Short Day, you should finish max at:", start + shortday)
     print("************************")
 
-calculate_info(datetime.timedelta(hours=8, minutes=0, seconds=0),  # start
-               datetime.timedelta(hours=15, minutes=0, seconds=0),  # end
+
+start_time = datetime.timedelta(hours=8, minutes=0, seconds=0)
+calculate_info(start_time,  # start
+               datetime.timedelta(hours=14, minutes=0, seconds=0),  # end
                PAUSE, BASIS, SHORTDAY)
 print("-------------------------------------------------------------------")
-calculate_leave(datetime.timedelta(hours=9, minutes=0, seconds=0),  # start
-               PAUSE, BASIS, SHORTDAY)
+calculate_leave(start_time,  # start
+                PAUSE, BASIS, SHORTDAY)
